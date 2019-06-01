32b66cd846914fa1e595cea6aff8c213e22385aa
rmb <rob@robertbryan.net>
2019-06-01 10:03:16 -0400

    safe checkpoint

--- p668.py
+++ p668.py
@@ -1,14 +1,6 @@
 import math
 import time
-primeset = []
-
-def primes(n):
-    """ Returns  a list of primes < n """
-    sieve = [True] * n
-    for i in range(3,int(n**0.5)+1,2):
-        if sieve[i]:
-            sieve[i*i::2*i]=[False]*((n-i*i-1)//(2*i)+1)
-    return [2] + [i for i in range(3,n,2) if sieve[i]]
+import multiprocessing
 
 
 def maxpfactor(n):
@@ -38,16 +30,6 @@ def is_root_smooth(i):
         return False
 
 
-# def fast_is_root_smooth(i):
-#     #is there a single prime factor of i greater than sqrt i?
-#     r = math.ceil(math.sqrt(i))
-#     for j in primes_range(r, i+1):
-#         if i % j == 0:
-#             # print("Not rs because ", j, " is a prime factor of ", i, " greater than ", r)
-#             return False
-#     return True
-
-
 def root_smooth_counter(y):
     counter = 0
     for i in range(1, y+1):
@@ -63,34 +45,7 @@ def rs_range_counter(a, b):
             counter += 1
     return counter
 
-
-def fast_rs_range_counter(a, b):
-    counter = 0
-    for i in range(a, b+1):
-        if fast_is_root_smooth(i):
-            counter += 1
-    return counter
-
-upperbound = 100000000
-
 start = time.time()
-primeset = primes(upperbound)
+print(rs_range_counter(1, 1000000))
 end = time.time()
-print(end - start, " seconds to populate the primes table up to ", upperbound)
-
-# start = time.time()
-# print(fast_rs_range_counter(1, upperbound))
-# end = time.time()
-# print(end - start, " seconds the new way.")
-#
-# start = time.time()
-# print(rs_range_counter(1, upperbound))
-# end = time.time()
-# print(end - start, " seconds the old way.")
-
-
-# for i in range(1, 100):
-#     if not (is_root_smooth(i) == fast_is_root_smooth(i)):
-#         print(i, is_root_smooth(i), fast_is_root_smooth(i))
-
-
+print(end - start, " seconds.")
