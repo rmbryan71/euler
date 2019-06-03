0d2572d7cfab08204a714e996685a0435b048efa
rmb <rob@robertbryan.net>
2019-06-03 05:37:00 -0400

    completes sudoku solver for p096

--- p096.py
+++ p096.py
@@ -5,18 +5,19 @@ cols = digits
 squares = []
 for row in rows:
     for col in cols:
-        squares.append(row+col)
+        squares.append(row + col)
+
+
 # print(squares)
 
 
 def cross(A, B):
-    return [a+b for a in A for b in B]
+    return [a + b for a in A for b in B]
 
 
 unitlist = ([cross(rows, c) for c in cols] +
             [cross(r, cols) for r in rows] +
-            [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')])
-
+            [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')])
 
 units = {}
 
@@ -37,55 +38,26 @@ for s in squares:
                 unit_set.add(square)
     peers[s] = unit_set
 
-print(peers['B4'])
-
 
 def test():
-    "A set of unit tests."
     assert len(squares) == 81
     assert len(unitlist) == 27
     assert all(len(units[s]) == 3 for s in squares)
     assert all(len(peers[s]) == 20 for s in squares)
     assert units['C2'] == [
-      ['A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2', 'I2'],
-      ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9'],
-      ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3']]
-    assert peers['C2'] == set(
-      ['A2', 'B2', 'D2', 'E2', 'F2', 'G2', 'H2', 'I2',
-       'C1', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9',
-       'A1', 'A3', 'B1', 'B3'])
+        ['A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2', 'I2'],
+        ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9'],
+        ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3']]
+    assert peers['C2'] == ['A2', 'B2', 'D2', 'E2', 'F2', 'G2', 'H2', 'I2', 'C1', 'C3',
+                           'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'A1', 'A3', 'B1', 'B3']
     print('All tests pass.')
-
-test()
-
-grid1 = "003020600900305001001806400008102900700000008006708200002609500800203009005010300"
-
-grid1_chars = []
-
-for c in grid1:
-    if c in digits or c in '0.':
-        grid1_chars.append(c)
-
-assert len(grid1_chars) == 81
-
-print(grid1_chars)
-
-grid1_values = {}
-#
-# for k, v in zip(squares, grid1_chars):
-#     grid1_values[k] = v
-#
 
 
 def grid_values(grid):
     "Convert grid into a dict of {square: char} with '0' or '.' for empties."
     chars = [c for c in grid if c in digits or c in '0.']
-    assert len(chars) == 81
+    #assert len(chars) == 81
     return dict(zip(squares, chars))
-
-grid1_values = grid_values(grid1)
-
-print(grid1_values['A1'])
 
 
 def parse_grid(grid):
@@ -93,9 +65,9 @@ def parse_grid(grid):
     return False if a contradiction is detected."""
     ## To start, every square can be any digit; then assign values from the grid.
     values = dict((s, digits) for s in squares)
-    for s,d in grid_values(grid).items():
+    for s, d in grid_values(grid).items():
         if d in digits and not assign(values, s, d):
-            return False ## (Fail if we can't assign d to square s.)
+            return False  ## (Fail if we can't assign d to square s.)
     return values
 
 
@@ -113,11 +85,11 @@ def eliminate(values, s, d):
     """Eliminate d from values[s]; propagate when values or places <= 2.
     Return values, except return False if a contradiction is detected."""
     if d not in values[s]:
-        return values ## Already eliminated
-    values[s] = values[s].replace(d,'')
+        return values  ## Already eliminated
+    values[s] = values[s].replace(d, '')
     ## (1) If a square s is reduced to one value d2, then eliminate d2 from the peers.
     if len(values[s]) == 0:
-        return False ## Contradiction: removed last value
+        return False  ## Contradiction: removed last value
     elif len(values[s]) == 1:
         d2 = values[s]
         if not all(eliminate(values, s2, d2) for s2 in peers[s]):
@@ -126,9 +98,9 @@ def eliminate(values, s, d):
     for u in units[s]:
         dplaces = [s for s in u if d in values[s]]
         if len(dplaces) == 0:
-            return False ## Contradiction: no place for this value
+            return False  ## Contradiction: no place for this value
         elif len(dplaces) == 1:
-        # d can only be in one place in unit; assign it there
+            # d can only be in one place in unit; assign it there
             if not assign(values, dplaces[0], d):
                 return False
     return values
@@ -136,10 +108,10 @@ def eliminate(values, s, d):
 
 def display(values):
     "Display these values as a 2-D grid."
-    width = 1+max(len(values[s]) for s in squares)
-    line = '+'.join(['-'*(width*3)]*3)
+    width = 1 + max(len(values[s]) for s in squares)
+    line = '+'.join(['-' * (width * 3)] * 3)
     for r in rows:
-        print(''.join(values[r+c].center(width)+('|' if c in '36' else '') for c in cols))
+        print(''.join(values[r + c].center(width) + ('|' if c in '36' else '') for c in cols))
         if r in 'CF':
             print(line)
     print()
@@ -152,11 +124,11 @@ def solve(grid):
 def search(values):
     "Using depth-first search and propagation, try all possible values."
     if values is False:
-        return False ## Failed earlier
+        return False  ## Failed earlier
     if all(len(values[s]) == 1 for s in squares):
-        return values ## Solved!
+        return values  ## Solved!
     ## Chose the unfilled square s with the fewest possibilities
-    n,s = min((len(values[s]), s) for s in squares if len(values[s]) > 1)
+    n, s = min((len(values[s]), s) for s in squares if len(values[s]) > 1)
     return some(search(assign(values.copy(), s, d)) for d in values[s])
 
 
@@ -166,4 +138,15 @@ def some(seq):
         if e: return e
     return False
 
-display(solve(grid1))
+
+def main():
+    with open('p096_sudoku.txt') as f:
+        data = f.readlines()
+        grids = [[row.strip() for row in data[10*k-9: 10*k]] for k in range(1, 51)]
+        for grid in grids:
+            display(solve(grid))
+
+
+
+if __name__== "__main__":
+  main()
