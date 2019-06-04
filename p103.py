d949855a35c08b8d9f36c44e1377227c61352f16
rmb <rob@robertbryan.net>
2019-06-04 17:27:43 -0400

    branching 103

--- p103.py
+++ p103.py
@@ -16,5 +16,25 @@ def is_special6(inputset): #test for N = 6
             return False
     return True
 
-testset = (11, 18, 19, 20, 22, 24)
-print(is_special6((testset)))
+
+def is_special7(inputset): #test for N = 7
+    if not len(inputset) == 7:
+        return False
+    for b in itertools.permutations(inputset, 4):
+        b = set(b)
+        c = set(inputset).symmetric_difference(set(b))
+        #print(b, c)
+        if set_sum(b) <= set_sum(c):
+            return False
+    return True
+
+
+range = [x for x in range(1, 26)]
+minsum = 500
+
+for test in itertools.combinations(range, 6):
+    if is_special6(test):
+        if set_sum(test) < minsum:
+            minsum = set_sum(test)
+            print(test, minsum)
+
