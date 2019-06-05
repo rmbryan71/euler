2e388457562c7564ce6814a4e03572c237e58ce5
rmb <rob@robertbryan.net>
2019-06-05 05:29:03 -0400

    checkpoint I think is accurate, adjusting to speed up

--- p103.py
+++ p103.py
@@ -1,4 +1,4 @@
-import itertools
+import itertools, time
 
 
 def set_sum(inputset):
@@ -8,33 +8,85 @@ def set_sum(inputset):
 def is_special6(inputset): #test for N = 6
     if not len(inputset) == 6:
         return False
-    for b in itertools.permutations(inputset, 4):
-        b = set(b)
-        c = set(inputset).symmetric_difference(set(b))
-        #print(b, c)
-        if set_sum(b) <= set_sum(c):
+    for x in itertools.permutations(inputset):
+        b = [x[0], x[1], x[2]]
+        c = [x[3], x[4], x[5]]
+        if sum(b) == sum(c):
             return False
-    return True
+
+    for x in itertools.permutations(inputset):
+        b = [x[0], x[1], x[2]]
+        c = [x[3], x[4]]
+        if sum(b) <= sum(c):
+            return False
+
+    for x in itertools.permutations(inputset):
+        b = [x[0], x[1]]
+        c = [x[2], x[3]]
+        if sum(b) == sum(c):
+            return False
+
+    for x in itertools.permutations(inputset):
+        b = [x[0], x[1]]
+        c = [x[2]]
+        if sum(b) <= sum(c):
+            return False
+        else:
+            return True
 
 
 def is_special7(inputset): #test for N = 7
     if not len(inputset) == 7:
         return False
-    for b in itertools.permutations(inputset, 4):
-        b = set(b)
-        c = set(inputset).symmetric_difference(set(b))
-        #print(b, c)
-        if set_sum(b) <= set_sum(c):
+
+    for x in itertools.permutations(inputset):
+        b = [x[0], x[1]]
+        c = [x[2]]
+        if sum(b) <= sum(c):
             return False
-    return True
+
+    for x in itertools.permutations(inputset):
+        b = [x[0], x[1]]
+        c = [x[3], x[2]]
+        if sum(b) == sum(c):
+            return False
+
+    for x in itertools.permutations(inputset):
+        b = [x[0], x[1], x[2]]
+        c = [x[3], x[4]]
+        if sum(b) <= sum(c):
+            return False
+
+    for x in itertools.permutations(inputset):
+        b = [x[0], x[1], x[2]]
+        c = [x[3], x[4], x[5]]
+        if sum(b) == sum(c):
+            return False
+
+    for x in itertools.permutations(inputset):
+        b = [x[0], x[1], x[2], x[3]]
+        c = [x[4], x[5], x[6]]
+        if sum(b) <= sum(c):
+            return False
+        else:
+            return True
 
 
-range = [x for x in range(1, 26)]
-minsum = 500
+# range = [x for x in range(11, 29)]
+# start = time.time()
+# for test in itertools.combinations(range, 6):
+#     if is_special6(test):
+#         print(test, sum(test))
+# print(int(time.time() - start), " seconds executions time.")
 
-for test in itertools.combinations(range, 6):
-    if is_special6(test):
-        if set_sum(test) < minsum:
-            minsum = set_sum(test)
-            print(test, minsum)
+range = [x for x in range(28, 46)]
+start = time.time()
+solutions = 0
+for test in itertools.combinations(range, 7):
+    if is_special7(test):
+        print(test, sum(test))
+        solutions += 1
+        if solutions == 10:
+            break
 
+print(int(time.time() - start), " seconds executions time.")
