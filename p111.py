2ae446f00f79c953b5897e6c0c3f3780179429ab
rmb <rob@robertbryan.net>
2019-06-08 09:05:31 -0400

    creates first original multiprocess main using list comprehension and map that I wrote solo.

---
+++ p111.py
@@ -0,0 +1,33 @@
+import math, sympy, time, multiprocessing
+
+
+def m(n, d): # max repeated digits(d) for an n-digit number
+    lower_bound = 10**(n-1)
+    upper_bound = 10**n - 1
+    result = 0
+    for x in sympy.primerange(lower_bound, upper_bound+1):
+        if str(x).count(str(d)) > result:
+            result = str(x).count(str(d))
+            # print(str(x), str(d), result)
+    return result
+
+
+def m10(d): # max repeated digits(d) for a 10 digit number
+    lower_bound = 10**9
+    upper_bound = 10**10 - 1
+    result = 0
+    for x in sympy.primerange(lower_bound, upper_bound+1):
+        if str(x).count(str(d)) > result:
+            result = str(x).count(str(d))
+            # print(str(x), str(d), result)
+    return result
+
+
+p = multiprocessing.Pool(4)
+
+start = time.time()
+print(list(map(m10, range(0, 10))))
+print(time.time() - start)
+
+
+
