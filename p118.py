7c85a0af7ef6e4020df611a01d02eccf3c8148ae
rmb <rob@robertbryan.net>
2019-06-12 07:34:39 -0400

    working on p118

--- p118.py
+++ p118.py
@@ -7,13 +7,31 @@ import operator as op
 
 
 def f(x):
-    return 1
+    perms = itertools.permutations(range(1, x + 1))
+    seps = itertools.combinations_with_replacement(['', ','], x-1)
+
+    for perm in perms:
+        print(perm)
+    for sep in seps:
+        print(sep)
+
+    for perm in perms:
+        for set in itertools.product(perm, seps):
+            for item in set:
+                print(item)
+
+def f1(x):
+    primes = sympy.primerange(2, x)
+    for set in itertools.combinations(primes, 4):
+        print(set)
+
+
 
 
 def main():
 
     start = time.time()
-    print(f(0))
+    print(f1(100))
     print(int(time.time() - start), " seconds to run.")
 
 
