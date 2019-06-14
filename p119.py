f95b6034f43f39ff9da9a3a36e4b178332a9d9c6
rmb <rob@robertbryan.net>
2019-06-14 05:33:38 -0400

    solves p119

--- p119.py
+++ p119.py
@@ -1,7 +1,7 @@
 import time
 import itertools
 import math
-# import sympy
+import sympy
 # import functools
 # import operator as op
 
@@ -30,8 +30,17 @@ def beta_is_power_of(y, x):  # need a faster check based on log y(x) is int
     return d == int(d)
 
 
+def is_valid(n):
+    dsum = sum(map(int, str(n)))
+    return dsum ** int(round(math.log(n, dsum))) == n if dsum > 1 else n < 2
+
+
+def sympy_test(x):
+    return sympy.is_Pow
+
+
 def is_dig_sum_pow(x):  # True if x is a digit sum power
-    return beta_is_power_of(dig_sum(x), x)
+    return is_power_of(dig_sum(x), x)
 
 
 def main():
@@ -48,6 +57,7 @@ def main():
     assert is_dig_sum_pow(614656)
     assert not is_dig_sum_pow(37)
     assert not is_dig_sum_pow(39847239874)
+    assert is_valid(3904305912313344)
 
     # a = []
     # for i in range(11, 614657):
@@ -65,14 +75,30 @@ def main():
     # print(a)
     # print(a[30])
 
-    a = []
-    i = 11
-    target = 12
-    while len(a) < target:
-        if is_dig_sum_pow(i):
-            a.append(i)
-        i += 1
-    print("a sub", target, "is", a[target - 1])
+    # a = []
+    # i = 11
+    # target = 12
+    # while len(a) < target:
+    #     if is_valid(i):
+    #         print(len(a), i, int(time.time() - start))
+    #         a.append(i)
+    #     i += 1
+    # print("a sub", target, "is", a[target - 1])
+
+    valid_list = []
+    while len(valid_list) < 31:
+        for a in range(100):
+            for b in range(100):
+                c = a**b
+                if len(str(c)) > 1:
+                    if dig_sum(c) == a:
+                        if c not in valid_list:
+                            valid_list.append(c)
+
+    valid_list.sort()
+    print(valid_list)
+    print(valid_list[30])
+
 
     print(int(time.time() - start), " seconds to run.")
 
