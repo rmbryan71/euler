bd977d332f09e719e9fe615a391eb409244d19ce
rmb <rob@robertbryan.net>
2019-06-15 11:38:53 -0400

    p125 is accurate but too slow

--- p124.py
+++ p124.py
@@ -5,13 +5,14 @@ import numpy
 def rad(x):
     return numpy.prod(sympy.primefactors(x))
 
-print(sorted(range(1,100001), key=rad)[10000-1])
 
-#
-# with open('p124.txt', 'w+') as f:
-#     for i in range(1, 100001):
-#         f.write(str(i) + "," + str(rad(i)))
-#         f.write('\n')
-#     f.close()
+with open('p124.txt', 'w+') as f:
+    for i in range(1, 100001):
+        f.write(str(i) + "," + str(rad(i)))
+        f.write('\n')
+    f.close()
+
+
+# print(sorted(range(1,100001), key=rad)[10000-1])
 
 
