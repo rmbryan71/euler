207373d0ee285b0ddd213b1e8d3be95e7743ec05
rmb <rob@robertbryan.net>
2019-06-16 05:52:53 -0400

    reverting to a solution that works.

--- p127.py
+++ p127.py
@@ -1,34 +1,23 @@
 import sympy, numpy
 import math
 import time
-import itertools
 
 def rad(x):
     return numpy.prod(sympy.primefactors(x))
 
 
-def is_hit(a, b, limit):
-    if a >= b:
-        return False
-    c = a + b
-    if c > limit:
-        return False
-    if math.gcd(a, b) == 1:
-        if math.gcd(b, c) == 1:
-            if math.gcd(a, c) == 1:
-                if rad(a * b * c) < c:
-                    return c
-    return False
-
-
-limit = itertools.repeat(1000)
-limit_int = 1000
-start = time.time()
-count = 0
-result = 0
-a = range(1, limit_int//2)
-b = range(1, limit_int//2)
-wins = map(is_hit, a, b, limit)
-wins = list(wins)
-
-print(len(wins), sum(wins), limit, int(time.time() - start))
+for limit in range(1000, 120500, 500):
+    start = time.time()
+    count = 0
+    result = 0
+    for a in range(1, limit//2):
+        for b in range(a + 1, limit - a):
+            c = a + b
+            if c < limit:
+                if math.gcd(a, b) == 1:
+                    if math.gcd(b, c) == 1:
+                        if math.gcd(a, c) == 1:
+                            if rad(a * b * c) < c:
+                                count += 1
+                                result += c
+    print(count, result, limit, int(time.time() - start))
