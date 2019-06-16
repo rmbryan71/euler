91d2162af6edfabd5166512548509d22586bda87
rmb <rob@robertbryan.net>
2019-06-15 23:59:18 -0400

    naieve, slow, accurate solution to 127, several orders of magnitude too slow

--- p127.py
+++ p127.py
@@ -1,9 +1,23 @@
 import sympy, numpy
-
+import math
+import time
 
 def rad(x):
     return numpy.prod(sympy.primefactors(x))
 
-print(numpy.gcd(5,27))
-print(rad(504))
 
+for limit in range(1000, 120500, 500):
+    start = time.time()
+    count = 0
+    result = 0
+    for a in range(1, limit//2):
+        for b in range(a + 1, limit - a):
+            c = a + b
+            if c < limit:
+                if math.gcd(a, b) == 1:
+                    if math.gcd(b, c) == 1:
+                        if math.gcd(a, c) == 1:
+                            if rad(a * b * c) < c:
+                                count += 1
+                                result += c
+    print(count, result, limit, int(time.time() - start))
