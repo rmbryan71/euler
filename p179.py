cd3a48b6fe931ac433629cd9b156cb1ee38d25d3
rmb <rob@robertbryan.net>
2019-07-04 06:28:17 -0400

    solves p179 correctly

--- p179.py
+++ p179.py
@@ -1,5 +1,8 @@
 import time
-import math
+from math import sqrt
+from multiprocessing import Pool
+from operator import itemgetter
+from functools import reduce
 
 
 def countfactors(x):
@@ -7,17 +10,28 @@ def countfactors(x):
     for i in range(1, x//2 + 1):
         if x % i == 0:
             count += 1
-    return count
+    return x, count
+
+
+def cfactors(n):
+        step = 2 if n % 2 else 1
+        return n, len(set(reduce(list.__add__,
+                          ([i, n // i] for i in range(1, int(sqrt(n)) + 1, step) if n % i == 0))))
 
 
 def main():
+    limit = 10**7 + 1
     start = time.time()
-    count = 0
-    for i in range(1, 1000):
-        if countfactors(i) == countfactors(i + 1):
-            count += 1
-            print(count, i)
+    factors = []
+    p = Pool()
+    factors.append(p.map(cfactors, range(1, limit)))
     end = time.time()
+    sorted_factors = sorted(factors, key=itemgetter(0))
+    count = 0
+    for i in (range(0, len(sorted_factors[0]) - 1)):
+        if sorted_factors[0][i][1] == sorted_factors[0][i + 1][1]:
+            count += 1
+
     print(end - start)
     print(count)
 
