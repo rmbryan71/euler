d4ff8995fd788e9ed590c7bb02fe3ceb280a10ee
rmb <rob@robertbryan.net>
2019-07-08 07:21:14 -0400

    working p346

--- p346.py
+++ p346.py
@@ -6,7 +6,7 @@ def tostr(n, base):
         return tostr(n//base, base) + convertstring[n % base]
 
 
-def f(x, y):  # return true if x is a repunit in base y
+def f(x, y):  # return True if x is a repunit in base y
     if x <= y:
         if x == 1:
             return True
@@ -18,3 +18,22 @@ def f(x, y):  # return true if x is a repunit in base y
         return f(x // y, y)
 
 
+def g(x):  # return True if x is a strong repunit
+    if x == 1:
+        return True
+    count = 0
+    for i in range(2, x):
+        count += f(x, i)
+        if count == 2:
+            return True
+    return False
+
+
+sum = 0
+for i in range(1, 10**12):
+    if g(i):
+        sum += i
+        print(i, sum, int(i*100)//10**12)
+print(sum)
+
+# Maybe it would be faster to make all the base 2 repunits, then base 3 repunits,
