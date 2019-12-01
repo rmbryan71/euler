138b870c546ddfe77e6cae470c0d712a4ad39514
Rob Bryan <rob@robertbryan.net>
2019-12-01 12:15:39 -0500

    correctly calculates the answer for the small example set

--- p345.py
+++ p345.py
@@ -1,7 +1,25 @@
-result = []
+import itertools
+from numpy import prod
+
+
+m = []
 inputfile = open('p345_example.txt', 'r')
 for line in inputfile.readlines():
-    result.append([int(x) for x in line.split(',')])
+    m.append([int(x) for x in line.split(',')])
+
+size = 5
+maxsum = 0
+rows = range(0, size)
+columns = range(0, size)
+for row in itertools.permutations(rows, size):
+    for column in itertools.permutations(columns, size):
+        k = []
+        for i in range(size):
+            k.append(m[column[i]][row[i]])
+        if sum(k) > maxsum:
+            maxsum = sum(k)
+print(maxsum)
+
 
 
 
